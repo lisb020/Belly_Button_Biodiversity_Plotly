ca5ddf3dd4246c18d87cc51e1e1398ae0207e04a
Lisa Stroh <lisb020@yahoo.com>
2021-02-17 21:34:26 -0800

    finished bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,80 +1,85 @@
+// clear dropdown list
+d3.select("#selDataset").html("");
+
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
 d3.json("data/samples.json").then((importedData) => {
     // console.log(importedData);
     let data = importedData;
     
-    d3.select("#selDataset").on("change",dropdown)
+    //load dropdown menu with sample names/people
+    data.names.forEach(name => d3.select("#selDataset").append("option").text(name).attr("value",name));
     
-//     // Sort the data array using the sample_values to get the top 10 OTUs
-//     data.sort(function(a, b) {
-//       return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-//     });
-//       // Slice the first 10 objects for plotting
-//   data = data.slice(0, 10);
+    // load sheet with the 940 data on refresh
+    let dataLoad = data.samples.filter(person => person.id === "940");
 
-//   // Reverse the array due to Plotly's defaults
-//   data = data.reverse();
-//     // Trace1 for the Data
-//   var trace1 = {
-//     x: data.map(row => row.greekSearchResults),
-//     y: data.map(row => row.greekName),
-//     text: data.map(row => row.greekName),
-//     name: "Greek",
-//     type: "bar",
-//     orientation: "h"
-//   };
-
-//   // data
-//   var chartData = [trace1];
-
-//   // Apply the group bar mode to the layout
-//   var layout = {
-//     title: "Greek gods search results",
-//     margin: {
-//       l: 100,
-//       r: 100,
-//       t: 100,
-//       b: 100
-//     }
-//   };
-
-//   // Render the plot to the div tag with id "plot"
-//   Plotly.newPlot("plot", chartData, layout);
-});
-
-function dropdown(){
-    let dataset = d3.select("#selDataset").property("value");
-    console.log(dataset);
-    let dataCompare = data.samples.filter(person => person.id === dataset);
-    console.log(dataCompare);
     // Sort the data array using the sample_values to get the top 10 OTUs
-    dataCompare.sort(function(a, b) {
+    dataLoad.sort(function(a, b) {
       return parseInt(b.sample_values) - parseInt(a.sample_values);
     });
-    console.log("sorted",dataCompare);
-      // Slice the first 10 objects for plotting
-    data = dataCompare.slice(0, 10);
-    console.log("sliced",data);
-    // let x = [];
-    // let y = [];
-    // switch (dataset){
-    //     case "dataset1":
-    //         x = [1, 2, 3, 4, 5];
-    //         y = [1, 2, 4, 8, 16];
-    //         break;
-    //     case "dataset2":
-    //         x = [10, 20, 30, 40, 50];
-    //         y = [1, 10, 100, 1000, 10000];
-    //         break;
-    //     case "dataset3":
-    //         x = [100, 200, 300, 400, 500];
-    //         y = [10, 100, 50, 10, 0];
-    //         break;
-    //     default:
-    //         x = [1, 2, 3, 4, 5];
-    //         y = [1, 2, 3, 4, 5];
-    // }
-    // Plotly.restyle("plot","x",[x]);
-    // Plotly.restyle("plot","y",[y]);
-    }
+
+    // Slice the first 10 objects of the sample_values for plotting
+    let sampleValues = parseInt(dataLoad.map(value => value.sample_values.slice(0,10)));
+    
+    // Slice the first 10 objects of the otu_ids for plotting
+    let otuIds = dataLoad.map(value => value.otu_ids.slice(0,10));
+    let otuIds2 = otuIds[0].map(value => "OTU " + String(value));
+    
+    // Slice the first 10 objects of the otu_labels for plotting
+    let otuLabels = dataLoad.map(value => value.otu_labels.slice(0,10));
+    
+
+    // create trace or horizontal bar chart
+    let trace1 = {
+        x: sampleValues[0],
+        y: otuIds2,
+        text: otuLabels,
+        name: "Bar",
+        type: "bar",
+        orientation: "h"
+    };
+
+    // data
+    let chartData = [trace1];
+
+    // Apply the layout
+    let layout = {
+        title: "Sample Values vs OTU IDs",
+    };
+
+    // Render the plot id "bar"
+    Plotly.newPlot("bar", chartData, layout);
+
+    d3.select("#selDataset").on("change",dropdown)
+    
+    function dropdown(){
+        let dataset = d3.select("#selDataset").property("value");
+        
+        // filter for the dropdown id value
+        let dataCompare = data.samples.filter(person => person.id === dataset);
+        
+        // Sort the data array using the sample_values to get the top 10 OTUs
+        dataCompare.sort(function(a, b) {
+          return parseInt(b.sample_values) - parseInt(a.sample_values);
+        });
+        
+        // Slice the first 10 objects of the sample_values for plotting
+        sampleValues = dataCompare.map(value => value.sample_values.slice(0,10));
+        
+        // Slice the first 10 objects of the otu_ids for plotting
+        otuIds = dataCompare.map(value => value.otu_ids.slice(0,10));
+        otuIds2 = otuIds[0].map(value => "OTU " + String(value));
+        
+        // Slice the first 10 objects of the otu_labels for plotting
+        otuLabels = dataCompare.map(value => value.otu_labels.slice(0,10));
+        
+        //Reverse the order of the sort and go into the 0th array
+        sampleValues = sampleValues[0].reverse()
+
+        Plotly.restyle("bar","y",[otuIds2]);
+        Plotly.restyle("bar","x",[sampleValues]);
+        }
+
+});
+
+
